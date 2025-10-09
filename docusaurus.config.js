2e2c4fb3a5214a9e63b2a59b16f8de1b562d666e
Filippo Ravazzini <filliravaz@gmail.com>
2025-10-09 10:03:45 +0000

    chore: Update NPM packages and docusaurus configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,11 @@ const config = {
   url: 'https://mcserversoft.github.io',
   baseUrl: '/',
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
   favicon: '/favicon.ico',
   organizationName: 'mcserversoft',
   projectName: 'documentation',
